e4a059e824c056977df6c8c2b3443a7a7cbf10e4
puneeth.kpati <puneeth.kpati@gmail.com>
2020-05-14 15:09:56 +1000

    forgot

---
+++ application.py
@@ -0,0 +1,46 @@
+
+class Application(Object):
+
+	def __init__(self):
+		self._algorithm = Algorithm()
+		self._array = []
+		self._sorted = []
+
+	def run(self):
+		self.printAlgorithmInstructions()
+		algorithmSelect = input()
+		self._algorithm = Algorithm(algorithmSelect)
+
+		self.printArrayInstructions()
+		inputArray = input()
+		if inputArray == None or inputArray == 0:
+			print('No Array detected...')
+			
+		self.sort()
+
+
+
+	def printAlgorithmInstructions(self):
+		print('Pick a sorting Algorithm:')
+		print('[1] : Insertion Sort')
+
+	def printArrayInstructions():
+		print('Enter Array: ')
+		print('[Example Format]:\n1,2,3,45,200')
+
+	def algorithm(self):
+		return self._algorithm
+
+	def setAlgorithm(self, algorithm): 
+		self._algorithm = algorithm
+		 
+	def array(self):
+		return self._array
+
+	def setArray(self, array):
+		if array == None:
+			return 
+		self._array = array
+
+runAlgorithms = Application()
+runAlgorithms.run()
