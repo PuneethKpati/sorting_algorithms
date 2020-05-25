6f28e177b6c4f3e2c953ec64de0345fd72382c6d
puneeth.kpati <puneeth.kpati@gmail.com>
2020-05-25 15:53:54 +1000

    super class for all algorithms added

---
+++ algorithm.py
@@ -0,0 +1,16 @@
+
+class Algorithm:
+	def __init__(self, time, space, name):
+		self._name = name
+		self._space = space
+		self._time  = time
+
+	def sort(self, array):
+		return array
+
+	def __str__(self):
+		res =  f'{self._name}:' + '\n'
+		res += 'Time Complexity: ' + self._time + '\n'
+		res += 'Space Complexity: ' + self._space
+
+		return res
