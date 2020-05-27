0d223b420221e4e7b0f59a782bbabc6d0b77ca53
puneeth.kpati <puneeth.kpati@gmail.com>
2020-05-27 11:59:28 +1000

    selection sort bugs sorted out

--- selectionSort.py
+++ selectionSort.py
@@ -4,10 +4,13 @@ class SelectionSort(Algorithm):
 
 	def __init__(self):
 		super().__init__('O(n^2)', 'O(1)', 'Selection Sort')
-
+ 
 	def sort(self, array):
+		if not array:
+			return None
+
 		# Traverse the subarray with unsorted elements
-		for sortDone in range(len(array)):
+		for sortDone in range(len(array)-1):
 			# find the max element in subarray
 			maxIndex = 0
 			for index in range(len(array)-sortDone):
@@ -16,7 +19,7 @@ class SelectionSort(Algorithm):
 
 			# swap the biggest element in subarray with
 			# last element in the subarray
-			array[len(array)-sortDone], array[maxIndex] =  array[maxIndex], array[len(array)-sortDone]
+			array[len(array)-sortDone-1], array[maxIndex] =  array[maxIndex], array[len(array)-sortDone-1]
 
 		return array
 
