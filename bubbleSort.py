8ec4564ac902b3673e3ba8ba3f8cc78e2b1e90fe
puneeth.kpati <puneeth.kpati@gmail.com>
2020-05-27 12:04:16 +1000

    took care of Null inputs and comments added

--- bubbleSort.py
+++ bubbleSort.py
@@ -6,9 +6,15 @@ class BubbleSort(Algorithm):
 		super().__init__('O(n^2)', 'O(1)', 'Bubble Sort')
 
 	def sort(self, array):
-		for sortedI in range(len(array)):
+		# End function if no array is passed
+		if not array:
+			return None 
 
+		# split array into subarrays that shrink after each iteration
+		for sortedI in range(len(array)):
+			# move the largest element in the subarray to the end of the subarray
 			for index in range(1, len(array)-sortedI):
+				# only move element to right if it is larger than next element
 				if array[index-1] > array[index]:
 					array[index], array[index-1] = array[index-1], array[index]
 
