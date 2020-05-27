bebda3b842710486d97c62cfb5328c45c48ada1c
puneeth.kpati <puneeth.kpati@gmail.com>
2020-05-27 12:06:48 +1000

    took care of Null inputs and comments added

--- insertionSort.py
+++ insertionSort.py
@@ -6,8 +6,14 @@ class InsertionSort(Algorithm):
 		super().__init__('O(n^2)', 'O(1)', 'Insertion Sort')
 
 	def sort(self, array):
+		# End function if no array is passed
+		if not array:
+			return None 
+
+		# only consider first 'index' number of elements and sort them	
 		for index in range(len(array)):
-			
+			# within the first 'index' elements sort the array
+			# by inserting the last element from subarray into appropriate place within. 
 			for shift in range(index, 0, -1):
 				if array[shift] < array[shift-1]:
 					array[shift], array[shift-1] = array[shift-1], array[shift]
